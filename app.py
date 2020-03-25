4d41823183ffb8e8a0203f92932f01694f44142c
andykamath <andy@andykamath.com>
2020-03-25 02:57:46 -0400

    changed ip

--- app.py
+++ app.py
@@ -33,4 +33,4 @@ def hello_world():
 if __name__ == '__main__':
     app.jinja_env.auto_reload = True
     app.config['TEMPLATES_AUTO_RELOAD'] = True
-    app.run(TEMPLATES_AUTO_RELOAD=True)
+    app.run(host='0.0.0.0', port=80)
